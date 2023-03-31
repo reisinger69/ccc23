0c385e8321d8db1cee154264401888d276fecc28
Jonfras <jonas.kreuzh@gmail.com>
2023-03-31 10:06:19 +0200

    initial

--- src/reisinger/htl/Main.java
+++ src/reisinger/htl/Main.java
@@ -3,6 +3,25 @@ package reisinger.htl;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        countCells("O-O-O-O-O-O-O-O-O-O-\n" +
+                "-O-O-O-O-O-O-O-O-O-O\n" +
+                "O-O-O-O-O-O-O-O-O-O-\n" +
+                "-O-O-O-O-O-O-O-O-O-O\n" +
+                "O-O-O-O-O-O-O-O-O-O-\n" +
+                "-O-O-O-O-O-O-O-O-O-O\n" +
+                "O-O-O-O-O-O-O-O-O-O-\n" +
+                "-O-O-O-O-O-O-O-O-O-O");
+    }
+
+    private static void countCells(String input) {
+        int counter = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char currentC = input.charAt(i);
+            if (currentC == 'O') {
+                counter++;
+            }
+        }
+        System.out.println("Cells: " + counter);
     }
 }
+
